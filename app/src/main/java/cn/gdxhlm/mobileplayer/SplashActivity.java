90b9dec791a93b3bd0c993821587c87b69ca7e93
gdxhlm <434483420@qq.com>
2018-11-18 19:39:57 +0800

    第一个知识点

--- app/src/main/java/cn/gdxhlm/mobileplayer/SplashActivity.java
+++ app/src/main/java/cn/gdxhlm/mobileplayer/SplashActivity.java
@@ -1,9 +1,10 @@
 package cn.gdxhlm.mobileplayer;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
